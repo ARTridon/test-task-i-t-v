418cf06e90da26f1547cc7ba5b5a6c64fa383863
ar.barabash <ar.barabash@insidedynamic.de>
2024-07-12 19:02:27 +0300

    fix format

--- src/app/(main)/_components/UserDeleteAllButton.tsx
+++ src/app/(main)/_components/UserDeleteAllButton.tsx
@@ -1,20 +1,19 @@
-import { Button } from "@/components/ui/button";
-import { useDeleteAllUserAction } from "@/hooks/useUserAction";
+import { useDeleteAllUserAction } from '@/hooks/useUserAction'
+
+import { Button } from '@/components/ui/button'
 
 type UserDeleteAllButtonPropsType = {
-  userCount: number;
-};
+  userCount: number
+}
 
-export const UserDeleteAllButton = ({
-  userCount,
-}: UserDeleteAllButtonPropsType) => {
-  const { mutate, isPending } = useDeleteAllUserAction();
+export const UserDeleteAllButton = ({ userCount }: UserDeleteAllButtonPropsType) => {
+  const { mutate, isPending } = useDeleteAllUserAction()
   if (!userCount) {
-    return null;
+    return null
   }
   return (
     <Button variant="default" disabled={isPending} onClick={() => mutate()}>
       Delete All User
     </Button>
-  );
-};
+  )
+}
--- src/app/(main)/_components/UserTable.tsx
+++ src/app/(main)/_components/UserTable.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-import { Pencil, Trash } from "lucide-react";
+import { Pencil, Trash } from 'lucide-react'
 
 import {
   type ColumnDef,
@@ -16,18 +16,20 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-} from "@tanstack/react-table";
+} from '@tanstack/react-table'
 
-import dayjs from "dayjs";
+import dayjs from 'dayjs'
 
-import { type UserSchemaType } from "@/schemas/user-schema";
+import { type UserSchemaType } from '@/schemas/user-schema'
 
-import { UserCreateAndUpdateDialog } from "@/app/(main)/_components/UserCreateAndUpdateDialog";
-import { UserDeleteDialog } from "@/app/(main)/_components/UserDeleteDialog";
-import { UserXLSXUpload } from "@/app/(main)/_components/UserXLSXUpload";
+import { UserCreateAndUpdateDialog } from '@/app/(main)/_components/UserCreateAndUpdateDialog'
+import { UserDeleteAllButton } from '@/app/(main)/_components/UserDeleteAllButton'
+import { UserDeleteDialog } from '@/app/(main)/_components/UserDeleteDialog'
+import { UserXLSXUpload } from '@/app/(main)/_components/UserXLSXUpload'
 
-import { TableColumnHeader } from "@/components/table-helpers/TableColumnHeader";
-import { Button } from "@/components/ui/button";
+import { TableColumnHeader } from '@/components/table-helpers/TableColumnHeader'
+import { TableWrapper } from '@/components/table-helpers/TableWrapper'
+import { Button } from '@/components/ui/button'
 import {
   Table,
   TableBody,
@@ -35,89 +37,69 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
-
-import { UserDeleteAllButton } from "@/app/(main)/_components/UserDeleteAllButton";
-import { TableWrapper } from "@/components/table-helpers/TableWrapper";
+} from '@/components/ui/table'
 
 type UserTablePropsType = {
-  users: UserSchemaType[];
-};
+  users: UserSchemaType[]
+}
 
 export const UserTable = ({ users }: UserTablePropsType) => {
-  const [rowSelection, setRowSelection] = useState({});
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [rowSelection, setRowSelection] = useState({})
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
 
-  const [data, setData] = useState<UserSchemaType[]>([]);
+  const [data, setData] = useState<UserSchemaType[]>([])
 
   const columns: ColumnDef<UserSchemaType>[] = [
     {
-      accessorKey: "name",
+      accessorKey: 'name',
       header: () => <TableColumnHeader title="Name" />,
-      cell: ({ row }) => <div className="w-[80px]">{row.getValue("name")}</div>,
+      cell: ({ row }) => <div className="w-[80px]">{row.getValue('name')}</div>,
       enableSorting: false,
       enableHiding: false,
     },
     {
-      accessorKey: "email",
+      accessorKey: 'email',
       header: () => <TableColumnHeader title="Email" />,
       cell: ({ row }) => {
         return (
           <div className="flex space-x-2">
-            <span className="max-w-[500px] truncate font-medium">
-              {row.getValue("email")}
-            </span>
+            <span className="max-w-[500px] truncate font-medium">{row.getValue('email')}</span>
           </div>
-        );
+        )
       },
     },
     {
-      accessorKey: "createdAt",
+      accessorKey: 'createdAt',
       header: () => <TableColumnHeader title="Created At" />,
       cell: ({ row }) => {
         return (
           <div className="flex space-x-2">
-            <span className="max-w-[500px] truncate font-medium">
-              {row.getValue("createdAt")}
-            </span>
+            <span className="max-w-[500px] truncate font-medium">{row.getValue('createdAt')}</span>
           </div>
-        );
+        )
       },
     },
 
     {
-      id: "actions",
+      id: 'actions',
       header: () => <TableColumnHeader title="Action" />,
       cell: ({ row }) => (
         <div className="flex items-center gap-3">
-          <UserCreateAndUpdateDialog
-            title="Create User"
-            defaultValues={row.original}
-          >
-            <Button
-              className="cursor-pointer"
-              variant="ghost"
-              size={"icon"}
-              asChild
-            >
+          <UserCreateAndUpdateDialog title="Create User" defaultValues={row.original}>
+            <Button className="cursor-pointer" variant="ghost" size={'icon'} asChild>
               <Pencil />
             </Button>
           </UserCreateAndUpdateDialog>
           <UserDeleteDialog id={row.original.id!} name={row.original.name}>
-            <Button
-              className="cursor-pointer text-red-600"
-              variant="ghost"
-              size={"icon"}
-              asChild
-            >
+            <Button className="cursor-pointer text-red-600" variant="ghost" size={'icon'} asChild>
               <Trash />
             </Button>
           </UserDeleteDialog>
         </div>
       ),
     },
-  ];
+  ]
 
   const table = useReactTable({
     data,
@@ -141,17 +123,17 @@ export const UserTable = ({ users }: UserTablePropsType) => {
     getSortedRowModel: getSortedRowModel(),
     getFacetedRowModel: getFacetedRowModel(),
     getFacetedUniqueValues: getFacetedUniqueValues(),
-  });
+  })
 
   useEffect(() => {
     if (users) {
       const data = users?.map((user) => ({
         ...user,
-        createdAt: dayjs(user.created_at).format("YYYY-MM-DD"),
-      }));
-      setData(data);
+        createdAt: dayjs(user.created_at).format('YYYY-MM-DD'),
+      }))
+      setData(data)
     }
-  }, [users]);
+  }, [users])
   return (
     <section className="h-screen max-h-screen space-y-4 p-3">
       <div className="flex w-full items-center justify-end gap-x-3">
@@ -163,12 +145,8 @@ export const UserTable = ({ users }: UserTablePropsType) => {
         <UserDeleteAllButton userCount={users.length} />
       </div>
       <div className="rounded-md border py-3">
-        <TableWrapper
-          table={table}
-          colSpan={columns.length}
-          notFoundText="User Not Found!"
-        />
+        <TableWrapper table={table} colSpan={columns.length} notFoundText="User Not Found!" />
       </div>
     </section>
-  );
-};
+  )
+}
